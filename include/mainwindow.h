d658442f85f053ca09bf7fa67703cc619a252e4c
jxcv0 <jmicoud02@gmail.com>
2022-09-11 23:14:15 +0100

    Make Windowing not bad

---
+++ include/mainwindow.h
@@ -0,0 +1,173 @@
+#ifndef MAINWINDOW_H_
+#define MAINWINDOW_H_
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+#ifndef OPENGL_LIBS
+#define OPENGL_LIBS
+#include "glad.h"
+#include <GLFW/glfw3.h>
+#endif
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include "keys.h"
+
+class MainWindow {
+public:
+
+  /*! \brief        Construct window with initial width and height.
+   *  \param width  Initial witdth of the window.
+   *  \param height Initial witdth of the window.
+   *  \param name   The name of the window.
+   */
+  inline MainWindow(float width, float height, std::string name)
+    : m_width(width)
+    , m_height(height)
+    , m_name(name)
+    , m_window(nullptr)
+  {
+    glfwInit();
+  }
+
+  /*! \brief Destroy the window and terminate GLFW.
+   */
+  ~MainWindow() {
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+  }
+
+  /*! \brief       Set GLFW window hints
+   *  \param hint  The window hint enum
+   *  \param value The new value of the hint
+   */
+  inline void set_hint(int hint, int value) const {
+    glfwWindowHint(hint, value);
+  }
+
+  /*! \brief Create and show the window.
+   */
+  void show() {
+    if (nullptr != m_window) {
+      return;
+    }
+
+    m_window = glfwCreateWindow(m_width,
+                                m_height,
+                                m_name.data(),
+                                nullptr,
+                                nullptr);
+    if (nullptr == m_window) {
+      perror("unable to create glfwwindow");
+      glfwTerminate();
+      return;
+    }
+
+    glfwMakeContextCurrent(m_window);
+    glfwSetFramebufferSizeCallback(m_window,
+      [](GLFWwindow* window, int width, int height){
+        glViewport(0, 0, width, height);
+      }
+    );
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+      perror("Unable to initialize GLAD");
+      glfwTerminate();
+      return;
+    }
+
+    glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    // depth testing
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    // face culling 
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+  }
+
+  /*! \brief  Check if the window should close.
+   *  \return true if window should close, otherwise returns false.
+   */
+  inline bool should_close() {
+    return glfwWindowShouldClose(m_window);
+  }
+
+  /*! \brief Swap front and back buffer of the window.
+   */
+  inline void swap_buffers() { glfwSwapBuffers(m_window); }
+
+  /*! \brief Process all pending events.
+   */
+  inline void poll_events() { glfwPollEvents(); }
+
+  /*! \brief Set the background color of the window.
+   */
+  inline void set_bg(float r, float g, float b, float a) {
+    glClearColor(r, g, b ,a);
+  }
+
+  /*! \brief Clear bit buffers.
+   */
+  inline void clear_buffers() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  }
+
+  /*! \brief Set cursor position callback function.
+   */
+  template<typename Function>
+  inline void set_cursor_position_callback(Function function) {
+    glfwSetCursorPosCallback(m_window, function);
+  }
+
+  /*! \brief Update input buffer.
+   */
+  void update_input() {
+    m_input_buffer = 0;
+    for (auto key : KEYS) {
+      if (glfwGetKey(m_window, key) == GLFW_PRESS) {
+        m_input_buffer |= key;
+      }
+    }
+  }
+
+  /*! \brief     Linear search through active keys on this window
+   *  \param key The key to check for.
+   *  \return    true if key is pressed, otherwise returns false;
+   */
+  bool key_pressed(int key) {
+    return (m_input_buffer & key) != 0;
+  }
+
+  /*! \brief     Get a projection matrix for this window.
+   *  \param fov The field of view.
+   *  \return    The projection matrix.
+   */
+  glm::mat4 projection_matrix(float fov) {
+    if (fov == m_fov) {
+      m_fov = fov;
+      m_perspective_matrix = glm::perspective(
+          glm::radians(m_fov),
+          ((float)m_width / (float)m_height),
+          0.1f,
+          100.0f);
+    }
+    return m_perspective_matrix;
+  }
+
+private:
+  GLFWwindow* m_window; // not std::unique_ptr as it is managed by glfw
+  float m_width;
+  float m_height;
+  std::string m_name;
+  int m_input_buffer;
+  float m_fov;
+  glm::mat4 m_perspective_matrix;
+};
+
+#endif // MAINWINDOW_H_
---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include "mainwindow.h"
+
+// void on_mouse(GLFWwindow *window, double xPosIn, double yPosIn);
+
+int main(int argc, char const *argv[])
+{
+  MainWindow main_window(1080, 600, std::string("main-window"));
+  main_window.set_hint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  main_window.set_hint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  main_window.set_hint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // main_window.set_cursor_position_callback(on_mouse);
+  main_window.show();
+
+  while (!main_window.should_close()) {
+    main_window.update_input();
+    if (main_window.key_pressed(GLFW_KEY_ESCAPE)) {
+      break;
+    } else if (main_window.key_pressed(GLFW_KEY_W)) {
+      std::cout << "W\n";
+    } else if (main_window.key_pressed(GLFW_KEY_A)) {
+      std::cout << "A\n";
+    }
+    main_window.poll_events();
+    main_window.clear_buffers();
+    main_window.swap_buffers();
+  }
+
+	return 0;
+}
+
+// void on_mouse(GLFWwindow *window, double x, double y) {
+	// xen::camera::update_aim(static_cast<float>(x), static_cast<float>(y), 0.1f);
+// }
