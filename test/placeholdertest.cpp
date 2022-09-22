a353c490268d61f6e83c373c9bdd78d350799e6f
jxcv0 <jmicoud02@gmail.com>
2022-09-22 20:38:49 +0100

    Fix code using old static function

--- test/placeholdertest.cpp
+++ test/placeholdertest.cpp
@@ -3,7 +3,8 @@
 #include <entitysubsystem.h>
 
 TEST(placeholdersuite, placeholdertest) {
-  auto entity = EntitySubsystem::instance().create_entity();
+  EntitySubsystem entity_subsystem;
+  auto entity = entity_subsystem.create_entity();
   EXPECT_EQ(entity, 0);
   ASSERT_EQ(7 * 6, 42);
 }
