0e664be0b67ed165bd3da11cdd8a5b09178067d8
jmicoud <jacob.micoud@zenitel.com>
2022-09-22 16:35:17 +0100

    Make xengine a library and link to tests

--- test/placeholdertest.cpp
+++ test/placeholdertest.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 
+#include <entitysubsystem.h>
+
 TEST(placeholdersuite, placeholdertest) {
+  auto entity = EntitySubsystem::instance().create_entity();
+  EXPECT_EQ(entity, 0);
   ASSERT_EQ(7 * 6, 42);
 }
