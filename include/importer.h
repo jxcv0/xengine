30d9f45df39ed40ebf88bb853ecefc8e9840dcc0
jmicoud <jacob.micoud@zenitel.com>
2022-10-17 13:44:02 +0100

    add inital impl

--- include/importer.h
+++ include/importer.h
@@ -1,4 +1,33 @@
 #ifndef IMPORTER_H_
 #define IMPORTER_H_
 
-#endif // IMPORTER_H_
+#include <filesystem>
+
+// #include "stb_image.h"
+// #include "texture.h"
+
+template <typename T>
+class Importer {
+ public:
+  /**
+   * @brief Construct an importer that will operate on a filepath.
+   *
+   * @param filepath The filepath of the data to import.
+   */
+  Importer(std::filesystem::path filepath)
+      : m_filepath(filepath) {};
+
+  ~Importer() = default;
+
+  /**
+   * @brief Import the data from the file.
+   *
+   * @return A pointer to the data
+   */
+  T* import();
+
+ private:
+  std::filesystem::path m_filepath;
+};
+
+#endif  // IMPORTER_H_
---
+++ test/importertests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include <importer.h>
+#include <texture.h>
+
+TEST(importertests, import) {
+  //Importer<Texture> tex_importer("assets/models/cyborg/cyborg_diffuse.png");
+  // auto ptr = tex_importer.import();
+  ASSERT_FALSE(true);
+}
