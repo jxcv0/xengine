e1231b6661cc63264bbadc4e46fd36cb1b59d4e9
jmicoud <jacob.micoud@zenitel.com>
2022-10-18 14:03:39 +0100

    add import_impl

--- include/importer.h
+++ include/importer.h
@@ -7,6 +7,34 @@
 #include "stb_image.h"
 #include "texture.h"
 
+/**
+ * @brief implementation of import function for Importer class.
+ */
+namespace import_impl {
+
+  /**
+   * @brief Unspecialized import function throws runtime error.
+   */
+  template <typename T> 
+  void import(T* dest, const std::filesystem::path& filepath) {
+    throw std::runtime_error("no implementation for importing type");
+  }
+
+  /**
+   * @brief Function specialization for importing a texture.
+   *
+   * @param texture A pointer to the destination texture instance.
+   * @param filepath The path to the file to import.
+   */
+  template<>
+  void import(Texture *texture, const std::filesystem::path& filepath) {
+    texture->mp_data =
+        stbi_load(filepath.c_str(), &texture->m_width, &texture->m_height,
+                  &texture->m_num_channels, 0);
+  }
+} // namespace importer_impl
+
+
 template <typename T>
 class Importer {
  public:
@@ -22,21 +50,19 @@ class Importer {
   /**
    * @brief Import the data from the file.
    *
-   * @return A pointer to the data
+   * @param allocator The allocating class for the new data.
+   * @return A pointer to the data.
    */
-  template <typename Allocator>
-  T* import(Allocator allocator);
+  template<typename Allocator>
+  T *import(Allocator &allocator) {
+    T *dest = allocator.allocate(sizeof(T)); 
+    import_impl::import(dest, m_filepath);
+    return dest;
+  }
 
  private:
   std::filesystem::path m_filepath;
 };
 
-// template<>
-//
-//   Texture *texture;
-//   texture->mp_data = stbi_load(m_filepath.c_str(), &texture->m_width,
-//                               &texture->m_height, &texture->m_num_channels,
-//                               0);
-//   return texture;
 
 #endif  // IMPORTER_H_
