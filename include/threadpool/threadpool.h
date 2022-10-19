be000cc1947367f33783da49758fa6f0567b4f85
jxcv0 <jmicoud02@gmail.com>
2022-10-19 20:36:42 +0100

    add threadpool dir

---
+++ include/threadpool/threadpool.h
@@ -0,0 +1,16 @@
+#ifndef THREADPOOL_H_
+#define THREADPOOL_H_
+
+#include <memory>
+#include <vector>
+#include <memory.h>
+
+class ThreadPool {
+ public:
+  ThreadPool() {
+  }
+
+ private:
+};
+
+#endif // THREADPOOL_H_
---
+++ test/threadpooltests.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+#include <threadpool.h>
+
+TEST(threadpooltests, threadpool) {
+}
