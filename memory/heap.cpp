e48517dd8703815bea58c23d4e1988dd91e57b1b
jxcv0 <jmicoud02@gmail.com>
2022-10-23 21:48:20 +0100

    add initial heap

---
+++ memory/heap.cpp
@@ -0,0 +1,9 @@
+#include "heap.cpp"
+
+// The "user space" heap
+static xen::byte* s_heap;
+
+void xen::heap_init(std::size_t n) {
+	s_heap = static_cast<xen::byte>std::malloc(n);
+}
+
---
+++ memory/heap.h
@@ -0,0 +1,34 @@
+#ifndef HEAP_H_
+#define HEAP_H_
+
+#include <cstdint.h>
+
+namespace xen {
+	using unsigned char = byte;
+	/**
+	 * @brief Initialize the preallocated heap with n bytes of memory.
+	 *
+	 * @param n The size of the heap.
+	 */
+	void heap_init(std::size_t n);
+
+	/**
+	 * @brief Allocate memory suitable to store an array of type T[n].
+	 *
+	 * @param n The number of elements in the array pointed to by p.
+	 * @return a pointer to the memory.
+	 */
+	template <typename T>
+	T* malloc(std::size_t n);
+
+	/**
+	 * @brief Free the memory allocated at p.
+	 *
+	 * @param p The pointer to the memory.
+	 * @param n The number of the elements pointed to by p.
+	 */
+	template <typename T>
+	void free(T *p, std::size_t n);
+};
+
+#endif // HEAP_H_
