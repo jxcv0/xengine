8ea5b1f4f902466cf21e4628045af2d6d48f9057
jxcv0 <jmicoud02@gmail.com>
2022-10-24 20:18:12 +0100

    clang format

--- test/resourcetests.cpp
+++ test/resourcetests.cpp
@@ -1,10 +1,16 @@
 #include <gtest/gtest.h>
 #include <resource.h>
 #include <texture.h>
+#include <mesh.h>
 
-TEST(resourcestests, resource_test) {
+TEST(resourcestests, texture) {
   Resource<Texture> tex_resource("assets/models/cyborg/cyborg_diffuse.png");
   auto tex = tex_resource.get();
   ASSERT_NE(tex, nullptr);
   ASSERT_EQ(tex->m_width, 1024);
+}
+
+TEST(resourcestests, mesh) {
+  Resource<Mesh> mesh_resource("assets/models/cube.obj");
+  auto mesh = mesh_resource.get();
 }
