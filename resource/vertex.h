cbeedbc26cb86a3dd3cfa82bfcb0912f6ca72416
jmicoud <jacob.micoud@zenitel.com>
2022-11-06 00:17:35 +0000

    use lin in vertex

--- resource/vertex.h
+++ resource/vertex.h
@@ -1,18 +1,18 @@
 #ifndef VERTEX_H_
 #define VERTEX_H_
 
-#include <glm/ext.hpp>
-#include <glm/glm.hpp>
+#include <vec3.h>
+#include <vec2.h>
 
 /**
  * @brief A single vertex.
  */
 struct Vertex {
-  glm::vec3 m_position;
-  glm::vec3 m_normal;
-  glm::vec2 m_tex_coord;
-  glm::vec3 m_tangent;
-  glm::vec3 m_bitangent;
+  Vec3 m_position;
+  Vec3 m_normal;
+  Vec2 m_tex_coord;
+  Vec3 m_tangent;
+  Vec3 m_bitangent;
 };
 
 #endif  // VERTEX_H_
