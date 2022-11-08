7adab4e7fc4fc20120a17eca8644a421b72e2f81
jxcv0 <jmicoud02@gmail.com>
2022-11-08 22:50:38 +0000

    add parsing tokens

--- resource/importer.cpp
+++ resource/importer.cpp
@@ -15,6 +15,12 @@ void import_impl::import(Texture *texture,
                                &texture->m_height, &texture->m_num_channels, 0);
 }
 
+static inline auto parse_mtllib(std::stringstream& line) {
+  std::string str;
+  line >> str;
+  return std::filesystem::path(str.c_str());
+}
+
 template<>
 void import_impl::import(Model *model, const std::filesystem::path& filepath) {
   if (filepath.extension() != ".obj") {
@@ -25,11 +31,25 @@ void import_impl::import(Model *model, const std::filesystem::path& filepath) {
     std::stringstream linestream(line);
     std::string line_token;
     linestream >> line_token;
-    if (line_token == "v") {
-      // ...
+    if (line_token == "#") {
+      // skip comments
+      continue;
+    } else if (line_token == "mtllib") {
+      auto mtllib_filepath = parse_mtllib(linestream);
+    } else if (line_token == "o") {
+      std::cout << "object group\n";
+    } else if (line_token == "v") {
+      std::cout << "vertex\n";
     } else if (line_token == "vt") {
-      // ...
+      std::cout << "tex coord\n";
+    } else if (line_token == "vn") {
+      std::cout << "normal\n";
+    } else if (line_token == "usemtl") {
+      std::cout << "material\n";
+    } else if (line_token == "s") {
+      std::cout << "smoothing\n";
+    } else if (line_token == "f") {
+      std::cout << "face\n";
     }
-    // and so on ...
   }
 }
