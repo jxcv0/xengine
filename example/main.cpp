8055eb992bef5d337cc1c02cc882704338ff0504
jxcv0 <jmicoud02@gmail.com>
2022-12-12 18:03:41 +0000

    update example

--- example/main.cpp
+++ example/main.cpp
@@ -30,35 +30,6 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
   auto shader =
       ShaderUtils::load("render/glsl/uber.vert", "render/glsl/uber.frag");
 
-  float vertices[] = {
-      0.5f,  0.5f,  0.0f, 1.0f, 1.0f,  // top right
-      0.5f,  -0.5f, 0.0f, 1.0f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,  // bottom left
-      -0.5f, 0.5f,  0.0f, 0.0f, 1.0f   // top left
-  };
-
-  unsigned int indices[] = {0, 1, 3, 1, 2, 3};
-
-  unsigned int vbo, vao, ebo;
-  glGenBuffers(1, &vbo);
-  glGenBuffers(1, &ebo);
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_DYNAMIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
   auto projection_matrix = window.projection_matrix(60);
   auto model_matrix = lin::rotate(Mat4(1), Vec3(1, 0, 0), lin::radians(-55));
 
@@ -77,10 +48,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
     shader.set_uniform("view", camera.view_matrix());
 
     mesh.draw();
-    glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)(0));
 
-    gl_print_error(std::cout, __FILE__, __LINE__);
     window.swap_buffers();
     window.poll_events();
   }
