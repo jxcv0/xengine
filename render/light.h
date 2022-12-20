d2e9dd629da564aafa5269e483f4f5d2cd67a91e
jxcv0 <jmicoud02@gmail.com>
2022-12-20 13:31:48 +0000

    add light

--- render/light.h
+++ render/light.h
@@ -1,6 +1,14 @@
 #ifndef LIGHT_H_
 #define LIGHT_H_
 
-class Light {};
+#include "vec3.h"
+
+struct Light {
+  Vec3 m_pos;
+  Vec3 m_col;
+  float m_const;
+  float m_lin;
+  float m_quad;
+};
 
 #endif  // LIGHT_H_
