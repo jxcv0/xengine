821ede7797723f6c21fe27cfd33934ff765262a2
jmicoud <jacob.micoud@zenitel.com>
2022-12-23 17:53:27 +0000

    update threadpool tests

--- threadpool/test/threadpooltests.cpp
+++ threadpool/test/threadpooltests.cpp
@@ -6,17 +6,16 @@
 pthread_mutex_t lock;
 int counter = 0;
 
-void add_one(void *arg) {
+void add_one() {
   pthread_mutex_lock(&lock);
-  int *c = static_cast<int *>(arg);
-  *c = *c + 1;
+  counter++;
   pthread_mutex_unlock(&lock);
 }
 
 TEST(threadpooltests, test) {
   threadpool::init();
   for (int i = 0; i < 1000; i++) {
-    threadpool::schedule_task(add_one, &counter);
+    threadpool::schedule_task(add_one);
   }
   threadpool::stop();
   ASSERT_EQ(counter, 1000);
