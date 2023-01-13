c0af704adac2e99dbf672f94a676dd1634047ee1
jxcv0 <jmicoud02@gmail.com>
2023-01-13 14:34:09 +0000

    add xml functions to collada

--- resource/collada.c
+++ resource/collada.c
@@ -1,13 +1,16 @@
 #include "collada.h"
 
 #include <stddef.h>
-#include "xmlparser.h"
+#include <string.h>
 
 #define LIBRARY_GEOMETRIES_START "<library_geometries>"
 #define LIBRARY_GEOMETRIES_END "</library_geometries>"
 
 #define GEOMETRY_START "<geometry>"
-#define GEOMETRY_END"</geometry>"
+#define GEOMETRY_END "</geometry>"
+
+#define MESH_START "<mesh>"
+#define MESH_END "</mesh>"
 
 /**
  * ----------------------------------------------------------------------------
@@ -21,14 +24,14 @@ char *find_xml_tag_start(const char *search_start, const char *tag) {
  */
 char *find_xml_tag_end(const char *search_start, const char *tag) {
   char *start = strstr(search_start, tag);
-  return start += strlen(tag) - 1;
+  return start += strlen(tag);
 }
 
 /**
  * ----------------------------------------------------------------------------
  */
 char *find_geometry_elements(const char *file, char **start, char **end) {
-  *start = find_xml_tag_end(file, LIBRARY_GEOMETRIES_START) + 1;
-  *end = find_xml_tag_start(*start, LIBRARY_GEOMETRIES_END) - 1;
+  *start = find_xml_tag_start(file, LIBRARY_GEOMETRIES_START);
+  *end = find_xml_tag_end(*start, LIBRARY_GEOMETRIES_END);
   return *start;
 }
