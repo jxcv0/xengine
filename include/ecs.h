909ae59c42c8f462d150cf152ebd8a693757ad71
jmicoud <jacob.micoud@zenitel.com>
2023-05-04 17:53:21 +0100

    add geometry_buf

--- include/ecs.h
+++ include/ecs.h
@@ -6,9 +6,6 @@
 #include "types.h"
 
 // First bit is used to specify if id is unused
-#define ID_UNUSED (1 << 31)
-#define ID_MAX (UINT32_MAX - 1)
-
 #define GEOMETRY (1 << 0)
 #define MATERIAL (1 << 1)
 #define POSITION (1 << 2)
@@ -17,11 +14,11 @@
 
 int ecs_init(size_t nent);
 
-void ecs_shutdown();
-
-int ecs_err();
+void ecs_shutdown(void);
 
 int ecs_create_entity(uint32_t *e);
+
+void ecs_delete_entity(uint32_t e);
 
 int ecs_add_component(uint32_t e, uint32_t type);
 
--- src/ecs.c
+++ src/ecs.c
@@ -1,11 +1,11 @@
 #include "ecs.h"
 
-#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <errno.h>
 
-static int ecs_errno;
-static handle_t counter = 1;
+#define ID_UNUSED (1 << 31)
+#define ID_MAX (UINT32_MAX - 1)
 
 static struct {
   uint32_t *data;
@@ -13,13 +13,18 @@ static struct {
   size_t max;
 } entity_buf;
 
+static struct {
+  struct geometry *data;
+  size_t count;
+  size_t max;
+} geometry_buf;
+
 /**
  * ----------------------------------------------------------------------------
  */
 int ecs_init(size_t nent) {
   entity_buf.data = calloc(nent, sizeof(uint32_t));
   if (entity_buf.data == NULL) {
-    ecs_errno = ECSERR_SYS;
     return -1;
   }
   for (size_t i = 0; i < nent; i++) {
@@ -34,12 +39,17 @@ int ecs_init(size_t nent) {
  * ----------------------------------------------------------------------------
  */
 int ecs_create_entity(uint32_t *e) {
-  if (entity_buf.data == NULL || e == NULL ||
-      entity_buf.count == entity_buf.max) {
+  if (entity_buf.data == NULL || e == NULL) {
+      errno = EINVAL;
     return -1;
   }
 
-  for (size_t i = 0; i < entity_buf.max; i++) {
+  if (entity_buf.count == entity_buf.max) {
+    // TODO errno?
+    return -1;
+  }
+
+  for (size_t i = 0; i < entity_buf.count; i++) {
     if ((entity_buf.data[i] & ID_UNUSED) != 0) {
       *e = i;
       entity_buf.data[i] ^= ID_UNUSED;
@@ -53,7 +63,16 @@ int ecs_create_entity(uint32_t *e) {
 /**
  * ----------------------------------------------------------------------------
  */
+void ecs_delete_entity(uint32_t e) {
+
+    // TODO delete this entities entries in component buffers
+    entity_buf.data[e] = ID_UNUSED;
+}
+
+/**
+ * ----------------------------------------------------------------------------
+ */
 handle_t new_handle(void) {
-  assert(counter != UINT64_MAX);
+  static handle_t counter = 1;
   return counter++;
 }
