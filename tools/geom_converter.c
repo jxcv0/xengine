62535a066193e3925a8a2c4009580c319e27dee0
jmicoud <jacob.micoud@zenitel.com>
2023-05-18 15:48:32 +0100

    revert geom_converter to single mesh only

--- tools/geom_converter.c
+++ tools/geom_converter.c
@@ -1,4 +1,4 @@
-#include <argp.h>
+#include <glad.h>
 #include <assert.h>
 #include <assimp/cimport.h>
 #include <assimp/material.h>
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
 
 #include "assets.h"
 
@@ -19,78 +20,33 @@
  * @brief This is the file that contains the relative filepaths of all the
  * meshes, textures and animations that make up the model.
  */
-FILE *model_file = NULL;
-char *model_name = NULL;
-char *in_file = NULL;
-char *out_dir = NULL;
-size_t out_dir_len = 0;
+static FILE *infile;
+static FILE *outfile;
 
 void process_node(struct aiNode *node, const struct aiScene *scene);
 void process_mesh(struct aiMesh *mesh, const struct aiScene *scene);
 char *process_material(struct aiMaterial *mat, enum aiTextureType);
 
-// command line options
-static struct argp_option options[] = {
-    {"in_file", 'i', "input_file", 0, "The file to convert", 0},
-    {"name", 'n', "model_name", 0, "The name of the resulting model", 0},
-    {"out_dir", 'd', "output_directory", 0,
-     "The directory to store the output files in", 0},
-    {0}};
-
-static error_t parse_opt(int key, char *argv, struct argp_state *state) {
-  static int arg_num = 0;
-  switch (key) {
-    case 'i':
-      in_file = argv;
-      break;
-    case 'n':
-      model_name = argv;
-      break;
-    case 'd':
-      out_dir = argv;
-      out_dir_len = strlen(out_dir);
-      break;
-    case ARGP_KEY_ARG:
-      if (arg_num > 3) {
-        argp_usage(state);
-      }
-      break;
-    case ARGP_KEY_END:
-      if (arg_num < 3) {
-        argp_usage(state);
-      }
-      break;
-    default:
-      return ARGP_ERR_UNKNOWN;
-  }
-  arg_num++;
-  return 0;
-}
-
 int main(int argc, char *argv[]) {
-  struct argp parser = {0};
-  parser.options = options;
-  parser.parser = parse_opt;
-  argp_parse(&parser, argc, argv, 0, 0, 0);
-
-  char model_filepath[256];
-  strncpy(model_filepath, out_dir, out_dir_len);
-  strncpy(&model_filepath[out_dir_len], "/", 2);
-
-  size_t len = strlen(model_name);
-  strncpy(&model_filepath[out_dir_len + 1], model_name, len);
-  strncpy(&model_filepath[out_dir_len + len + 1], ".model", 7);
-
-  printf("Creating model file: %s\n", model_filepath);
-  model_file = fopen(model_filepath, "wb+");
-  if (model_file == NULL) {
-    perror("fopen");
+  if (argc != 3) {
+      printf("Usage:\n\tgeom_converter <infile> <outfile>\n");
+      exit(EXIT_FAILURE);
   }
 
-  printf("Outputting files to %s\n", out_dir);
+  infile = fopen(argv[1], "rb");
+  if (infile == NULL) {
+      perror("Unable to open input file");
+      exit(EXIT_FAILURE);
+  }
+
+  outfile = fopen(argv[2], "wb+");
+  if (outfile == NULL) {
+      perror("Unable to open output file");
+      exit(EXIT_FAILURE);
+  }
 
   const struct aiScene *scene =
-      aiImportFile(in_file, aiProcess_Triangulate | aiProcess_GenSmoothNormals |
+      aiImportFile(argv[1], aiProcess_Triangulate | aiProcess_GenSmoothNormals |
                                 aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
 
   if (scene == NULL) {
@@ -101,67 +57,65 @@ int main(int argc, char *argv[]) {
   process_node(scene->mRootNode, scene);
 
   aiReleaseImport(scene);
-  fclose(model_file);
+  fclose(infile);
   return 0;
 }
 
-/**
- * ============================================================================
- */
 void process_node(struct aiNode *node, const struct aiScene *scene) {
-  for (unsigned int i = 0; i < node->mNumMeshes; i++) {
-    struct aiMesh *m = scene->mMeshes[node->mMeshes[i]];
-    process_mesh(m, scene);
+  if (node->mNumMeshes != 1) {
+      fprintf(stderr, "Only single mesh models supported. %u found.\n", node->mNumMeshes);
+      exit(EXIT_FAILURE);
   }
 
-  for (unsigned int i = 0; i < node->mNumChildren; i++) {
-    process_node(node->mChildren[i], scene);
-  }
+  struct aiMesh *m = scene->mMeshes[node->mMeshes[0]];
+  process_mesh(m, scene);
 }
 
-/**
- * ============================================================================
- */
 void process_mesh(struct aiMesh *mesh, const struct aiScene *scene) {
-  uint32_t num_vertices = mesh->mNumVertices;
-  uint32_t num_indices = mesh->mNumFaces * 3;
+  (void) scene;
+  size_t num_vertices = mesh->mNumVertices;
+  size_t num_indices = mesh->mNumFaces * 3;
+
+
   size_t vertices_size = sizeof(struct vertex) * num_vertices;
-  size_t indices_size = sizeof(uint32_t) * num_indices;
+  size_t indices_size = sizeof(GLuint) * num_indices;
   struct vertex *vertices = malloc(vertices_size);
-  uint32_t *indices = malloc(indices_size);
+  GLuint *indices = malloc(indices_size);
 
   char *mesh_name = mesh->mName.data;
-  printf("\nLoading mesh: %s.\n", mesh_name);
+
+  printf("Loading mesh: %s.\n", mesh_name);
   printf("Copying %d vertices.\n", mesh->mNumVertices);
 
   for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
     struct vertex *v = &vertices[i];
 
-    v->m_position[0] = mesh->mVertices[i].x;
-    v->m_position[1] = mesh->mVertices[i].y;
-    v->m_position[2] = mesh->mVertices[i].z;
+    v->position[0] = mesh->mVertices[i].x;
+    v->position[1] = mesh->mVertices[i].y;
+    v->position[2] = mesh->mVertices[i].z;
 
     // if no texture coords are specified in the source file this will fail.
     assert(mesh->mTangents != NULL);
 
-    v->m_tangent[0] = mesh->mTangents[i].x;
-    v->m_tangent[1] = mesh->mTangents[i].y;
-    v->m_tangent[2] = mesh->mTangents[i].z;
+    v->tangent[0] = mesh->mTangents[i].x;
+    v->tangent[1] = mesh->mTangents[i].y;
+    v->tangent[2] = mesh->mTangents[i].z;
 
-    v->m_bitangent[0] = mesh->mBitangents[i].x;
-    v->m_bitangent[1] = mesh->mBitangents[i].y;
-    v->m_bitangent[2] = mesh->mBitangents[i].z;
+    v->bitangent[0] = mesh->mBitangents[i].x;
+    v->bitangent[1] = mesh->mBitangents[i].y;
+    v->bitangent[2] = mesh->mBitangents[i].z;
 
-    v->m_normal[0] = mesh->mNormals[i].x;
-    v->m_normal[1] = mesh->mNormals[i].y;
-    v->m_normal[2] = mesh->mNormals[i].z;
+    v->normal[0] = mesh->mNormals[i].x;
+    v->normal[1] = mesh->mNormals[i].y;
+    v->normal[2] = mesh->mNormals[i].z;
 
     if (mesh->mTextureCoords[0] != 0) {
-      v->m_tex_coord[0] = mesh->mTextureCoords[0][i].x;
-      v->m_tex_coord[1] = mesh->mTextureCoords[0][i].y;
+      v->tex_coord[0] = mesh->mTextureCoords[0][i].x;
+      v->tex_coord[1] = mesh->mTextureCoords[0][i].y;
     } else {
-      v->m_tex_coord[0] = 0;
-      v->m_tex_coord[1] = 0;
+      // TODO we might want invisible meshes?
+      v->tex_coord[0] = 0;
+      v->tex_coord[1] = 0;
     }
   }
 
@@ -173,77 +127,13 @@ void process_mesh(struct aiMesh *mesh, const struct aiScene *scene) {
     }
   }
 
-  printf("Loading materials for mesh.\n");
-  struct aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
-  char *diffuse_name = process_material(material, aiTextureType_DIFFUSE);
-  char *roughness_name = process_material(material, aiTextureType_SHININESS);
-  char *normal_name = process_material(material, aiTextureType_HEIGHT);
-  char *metallic_name = process_material(material, aiTextureType_METALNESS);
+  printf("Writing to output file...\n");
 
-  char filename[256];
-  size_t mesh_name_len = strlen(mesh_name);
-  strncpy(filename, out_dir, out_dir_len);
-  strncpy(&filename[out_dir_len], "/", 2);
-  strncpy(&filename[out_dir_len + 1], mesh_name, mesh_name_len);
-  strncpy(&filename[out_dir_len + mesh_name_len + 1], ".geom", 6);
+  fprintf(outfile, "VERTICES %ld\n", vertices_size);
+  fwrite(vertices, sizeof(struct vertex), num_vertices, outfile);
+  fprintf(outfile, "\n");
 
-  printf("Writing to file %s.\n", filename);
-  FILE *file = fopen(filename, "wb+");
-  if (file == NULL) {
-    perror("fopen");
-  }
-
-  fprintf(file, "VERTICES %d\n", num_vertices);
-  fwrite(vertices, sizeof(struct vertex), num_vertices, file);
-  fprintf(file, "\n");
-
-  fprintf(file, "INDICES %d\n", num_indices);
-  fwrite(indices, sizeof(uint32_t), num_indices, file);
-  fprintf(file, "\n");
-  fclose(file);
-
-  fprintf(model_file, "%s.geom %s %s %s %s\n", mesh_name, diffuse_name,
-          roughness_name, normal_name, metallic_name);
-}
-
-/*
- * ============================================================================
- */
-char *process_material(struct aiMaterial *mat, enum aiTextureType type) {
-  if (mat != NULL) {
-    unsigned int tex_count = aiGetMaterialTextureCount(mat, type);
-    for (unsigned int i = 0; i < tex_count; i++) {
-      struct aiString str;
-      int res = aiGetMaterialString(mat, AI_MATKEY_TEXTURE(type, i), &str);
-      if (res != AI_SUCCESS) {
-        fprintf(stderr, "error while getting texture\n");
-        exit(0);
-      }
-
-      char *texture_filename = malloc(str.length + 1);
-      strncpy(texture_filename, str.data, str.length);
-      texture_filename[str.length] = 0;
-      printf("Found texture %s.\n", texture_filename);
-      return texture_filename;
-    }
-  }
-
-  char *tex = malloc(64);
-  switch (type) {
-    case aiTextureType_DIFFUSE:
-      strcpy(tex, "default_diffuse.png");
-      break;
-    case aiTextureType_SHININESS:
-      strcpy(tex, "default_roughness.png");
-      break;
-    case aiTextureType_HEIGHT:
-      strcpy(tex, "default_normal.png");
-      break;
-    case aiTextureType_METALNESS:
-      strcpy(tex, "default_metallic.png");
-      break;
-    default:
-      break;
-  }
-  return tex;
+  fprintf(outfile, "INDICES %ld\n", indices_size);
+  fwrite(indices, sizeof(GLuint), num_indices, outfile);
+  fprintf(outfile, "\n");
 }
