3696c099f763dea047984e8798a2308962181dd1
jmicoud <jacob.micoud@zenitel.com>
2023-06-27 18:23:26 +0100

    add test test

--- include/test.h
+++ include/test.h
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <time.h>
 
-#define TEST_BEGIN() printf("%s: Running tests.\n", __FILE__)
-#define TEST_END() printf("%s: Tests passed.\n", __FILE__)
-#define TEST() printf("\t- %s\n", __func__)
+#define TEST_PASS 0
+#define TEST_SKIP 77
+#define TEST_FAIL 99
 
 #define ASSERT_FLOAT_EQ(f1, f2) assert(fabs(f1 - f2) < (FLT_EPSILON * 10))
+
+void test_pass(const char *file, const char *msg, ...) {}
 
 void print_time(clock_t start, const char *msg) {
   printf("TIME: %s - %f s\n", msg, (double)(clock() - start) / CLOCKS_PER_SEC);
---
+++ test/test_tests.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test.h"
+
+int main() { exit(TEST_PASS); }
