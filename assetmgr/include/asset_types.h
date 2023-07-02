f15aa42b979240aa9867f3cd3c67e1461e52bfbc
jxcv <jmicoud02@gmail.com>
2023-07-02 22:17:30 +0100

    add lib

---
+++ assetmgr/include/asset_types.h
@@ -0,0 +1,23 @@
+#ifndef ASSET_TYPES_H_
+#define ASSET_TYPES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct mesh {
+  float *vertices;
+  unsigned int *indices;
+  unsigned int nvertices;
+  unsigned int nindices;
+};
+
+struct texture {
+  unsigned char *img;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // ASSET_TYPES_H_
