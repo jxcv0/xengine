3299475a99c4a269ae8aa2a96c037c4aa645e824
jmicoud <jacob.micoud@zenitel.com>
2023-07-11 16:06:40 +0100

    DRY

--- ecs2/ecs.h
+++ ecs2/ecs.h
@@ -18,7 +18,11 @@ public:
         return m_mgr.create_entity();
     }
 
-    void delete_entity ();
+    void delete_entity (std::uint64_t entity)
+    {
+        m_mgr.delete_entity(entity);
+        std::for_each(m_arrays.begin(), m_arrays.end(), [entity](auto *a) { a->remove (entity); });
+    }
 
     template <typename T>
     void create_component()
@@ -30,17 +34,35 @@ public:
     void add_component (std::uint64_t entity)
     {
         m_mgr.add_component<T> (entity);
-        auto it = std::find_if (m_arrays.begin(), m_arrays.end(), [] (const auto *ca) { return ca->component_id () == typeid(T).hash_code(); });
-        if (it == m_arrays.end())
-        {
-            throw std::runtime_error("Component type does not exist");
-        }
-        (*it)->assign(entity);
+        auto arr = find_component_array<T>();
+        arr->assign(entity);
     }
 
     template <typename T>
     void remove_component(std::uint64_t entity)
     {
+        auto arr = find_component_array<T>();
+        arr->remove(entity);
+    }
+
+    template <typename T>
+    bool has_component(std::uint64_t entity)
+    {
+        find_component_array<T>();
+        return m_mgr.has_component<T>(entity);
+    }
+
+private:
+    template <typename T>
+    component_array_base *
+    find_component_array ()
+    {
+        auto it = std::find_if (m_arrays.begin(), m_arrays.end(), [] (const auto *ca) { return ca->component_id () == typeid(T).hash_code(); });
+        if (it == m_arrays.end())
+        {
+            throw std::runtime_error("Component type does not exist");
+        }
+        return *it;
     }
 
 private:
--- ecs2/test/ecs_tests.cpp
+++ ecs2/test/ecs_tests.cpp
@@ -6,10 +6,19 @@ struct position
     float xyz[3];
 };
 
-TEST (ecs_tests, create_and_assign)
+TEST (ecs_tests, create_assign_remove)
 {
     xen::ecs ecs;
     auto e1 = ecs.create_entity();
+    ASSERT_THROW (ecs.has_component<position>(e1), std::runtime_error);
     ecs.create_component<position>();
+    ASSERT_FALSE (ecs.has_component<position>(e1));
     ecs.add_component<position>(e1);
+    ASSERT_TRUE (ecs.has_component<position>(e1));
 }
+
+TEST (ecs_tests, create)
+{
+
+}
+
