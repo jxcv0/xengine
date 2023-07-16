012412f536f8cdfea5977d1b3cf61fe93b13cfe2
jxcv <jmicoud02@gmail.com>
2023-07-16 15:05:53 +0100

    add offset_map to chunks

--- ecs2/archetype.hpp
+++ ecs2/archetype.hpp
@@ -4,7 +4,7 @@
 #include "types.hpp"
 #include <cstddef>
 #include <cstdint>
-#include <type_traits>
+#include <typeindex>
 #include <vector>
 
 namespace xen
@@ -34,7 +34,42 @@ template <typename... ComponentTs>
 struct chunk
 {
   static const std::size_t size = (sizeof(ComponentTs) + ...);
+
+  /**
+   * @brief Construct a new chunk object assigned to an entity. Also populate
+   * the offset map with byte offsets for each type.
+   *
+   * @param entity The entity that owns this chunk.
+   */
+  chunk(eid_t entity) : m_entity{ entity }
+  {
+    std::size_t offset = 0;
+    (
+        [&] {
+          m_offset_map[std::type_index(typeid(ComponentTs))] = offset;
+          offset += sizeof(ComponentTs);
+        }(),
+        ...);
+  }
+
+  template <typename Component>
+  Component&
+  get_component()
+  {
+    auto* ptr = &bytes[m_offset_map[std::type_index(typeid(Component))]];
+    return *reinterpret_cast<Component*>(ptr);
+  }
+
+  eid_t
+  entity() const
+  {
+    return m_entity;
+  }
+
+public:
+  eid_t m_entity;
   std::byte bytes[size];
+  std::unordered_map<std::type_index, std::size_t> m_offset_map;
 };
 
 /**
@@ -69,23 +104,25 @@ class archetype
 {
 public:
   /**
-   * @brief Create an archetype.
+   * @brief Construct a new archetype with storage for all types in a parameter
+   * pack. All types in the parameter pack mush be unique.
    *
-   * @tparam ComponentTs The types that all entities of this archetype have an
-   * instance of.
+   * @tparam Component The first component type of the parameter pack.
+   * @tparam ComponentTs The rest of the component types of the parameter pack.
    * @return archetype The new archetype.
    */
   template <typename Component, typename... ComponentTs>
   static archetype
   create()
   {
-    static_assert((!std::is_same_v<Component, ComponentTs> && ...), "Duplicate types in archetype");
+    static_assert((!std::is_same_v<Component, ComponentTs> && ...),
+                  "Duplicate types in archetype");
     return archetype(new archetype_storage<Component, ComponentTs...>);
   }
 
   /**
    * @brief Get the size of the memory chunk required to store one instance of
-   * each type
+   * each type.
    *
    * @return std::size_t
    */
--- ecs2/test/archetype_tests.cpp
+++ ecs2/test/archetype_tests.cpp
@@ -2,6 +2,14 @@
 
 #include "archetype.hpp"
 
+TEST(chunk_tests, create)
+{
+  xen::chunk<int, float, char> chunk(0);
+  int& i = chunk.get_component<int>();
+  i = 10;
+  ASSERT_EQ(chunk.get_component<int>(), i);
+}
+
 TEST(archetype_tests, create)
 {
   xen::archetype arch = xen::archetype::create<int, float, char>();
