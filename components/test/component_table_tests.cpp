d8819c41dcb8427b97e3c442c1b4c80f1e950ccb
jxcv <jmicoud02@gmail.com>
2023-07-24 15:39:03 +0100

    remove process join

--- components/test/component_table_tests.cpp
+++ components/test/component_table_tests.cpp
@@ -29,3 +29,34 @@ TEST(component_table_tests, iterator) {
   ASSERT_TRUE(table.cbegin() < table.cbegin() + 1);
   ASSERT_TRUE(table.cend() > table.cbegin() + 1);
 }
+
+TEST(component_table_tests, use_case) {
+  xen::component_table<int> dest;
+  dest.insert(1, 10);
+  dest.insert(2, 20);
+  dest.insert(3, 30);
+
+  xen::component_table<int> src;
+  src.insert(0, 0);
+  src.insert(1, 10);
+  src.insert(3, 30);
+
+  [&] {
+    auto dest_it = dest.begin();
+    auto src_it = src.begin();
+    while (dest_it != dest.end() && src_it != src.end()) {
+      if (dest_it == src_it) {
+        *dest_it += *src_it;
+        ++dest_it;
+        ++src_it;
+      } else {
+        if (dest_it < src_it) ++dest_it;
+        if (dest_it > src_it) ++src_it;
+      }
+    }
+  }();
+
+  ASSERT_EQ(dest[0], 20);
+  ASSERT_EQ(dest[1], 20);
+  ASSERT_EQ(dest[2], 60);
+}
